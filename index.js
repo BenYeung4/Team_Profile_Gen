77a62cfc45b34dbb03ff59429ace506ff0b6d900
Ben <byeungis@gmail.com>
2022-06-21 16:33:35 -0400

    update

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const inquirer = require("inquirer");
-
+//generated page
 const pagetemplate = require("./src/page-template");
 
+//library
+const Engineer = require("./lib/Engineer");
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
+
+//node mods
+const inquirer = require("inquirer");
 const util = rquire("util");
 const fs = rquire("fs");
+
+//team array
+const team = [];
 
 const questions = [
   {
